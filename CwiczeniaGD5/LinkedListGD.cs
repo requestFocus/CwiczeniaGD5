ecee95bc3f8ec2ee34e1a4a62aa5078145ad68e4
requestFocus <thezodiac@wp.pl>
2017-10-02 19:49:58 +0200

    klasy w osobnych plikach, wysprzątane, brak ogarniętych modyfikatorów

---
+++ CwiczeniaGD5/LinkedListGD.cs
@@ -0,0 +1,179 @@
+﻿using System;
+
+namespace CwiczeniaGD5 {
+
+	class LinkedListGD {
+
+		public NodeGD CurrentNode;
+		public int Count;
+
+		public LinkedListGD() {
+
+			CurrentNode = null;
+		}
+
+		public class NodeGD {
+
+			public NodeGD Previous;
+			public NodeGD Next;
+			public int Value;
+		}
+
+		//================AddNodeGD
+
+		private void AddNodeGD(int nodeValue, int position) {
+
+			NodeGD newNode = new NodeGD();
+
+			if (Count != 0) {											// lista istnieje 
+
+				if (position == 0) {                    // ELEMENT NA POCZATKU
+
+					NodeGD MiddleToHead = CurrentNode;
+					GoToStart(ref MiddleToHead);
+
+					newNode.Previous = null;
+					newNode.Next = MiddleToHead;
+					MiddleToHead.Previous = newNode;
+					newNode.Value = nodeValue;
+					CurrentNode = newNode;
+				}
+
+				else if (position == Count) {           // ELEMENT NA KONCU
+
+					NodeGD MiddleToEnd = CurrentNode;
+					GoToStart(ref MiddleToEnd);
+
+					while (MiddleToEnd.Next != null) {                  // przesuwa na ostatni element listy
+						MiddleToEnd = MiddleToEnd.Next;
+					}
+
+					newNode.Previous = MiddleToEnd;
+					newNode.Next = null;
+					newNode.Value = nodeValue;
+					CurrentNode = newNode;
+				}
+
+				else {                                  // ELEMENT W SRODKU
+
+					NodeGD HeadToMiddle = CurrentNode;
+					GoToStart(ref HeadToMiddle);
+
+					for (int i = 0; i < position; i++) {                // przesuwa na wskazana pozycje
+						HeadToMiddle = HeadToMiddle.Next;
+					}
+
+					newNode.Previous = HeadToMiddle.Previous;
+					newNode.Next = HeadToMiddle;
+					HeadToMiddle.Previous = newNode;
+					newNode.Value = nodeValue;
+					CurrentNode = newNode;
+				}
+
+			}
+			else {
+				newNode.Previous = null;
+				newNode.Next = null;
+				newNode.Value = nodeValue;
+				CurrentNode = newNode;
+			}
+			Count++;
+
+			//PrintListGD(ref CurrentNode, Count);				// aby umożliwić wypisanie wszystkich kroków
+		}
+
+		//================GoToStart
+
+		private void GoToStart(ref NodeGD item) {
+
+			NodeGD ItemPosition = item;
+			NodeGD temp = ItemPosition;
+			while (ItemPosition.Previous != null) {
+				temp = ItemPosition;                            // do zmiennej tymczasowej leci aktualna pozycja HEAD
+				ItemPosition = ItemPosition.Previous;           // do aktualnej pozycji HEAD leci poprzednia pozycja HEAD
+				ItemPosition.Next = temp;
+			}
+			item = ItemPosition;
+		}
+
+		//================DeleteNodeGD
+
+		private void DeleteNodeGD(int nodeValue) {
+
+			//bool nodeFound = false;							// jeśli istotne jest sprawdzenie czy element do usunięcia występował na liście
+
+			NodeGD NodeToDelete = CurrentNode;
+			GoToStart(ref NodeToDelete);
+
+			while (NodeToDelete != null) {                      // sprawdzaj dopoki aktualnie sprawdzany node nie jest nullem
+
+				if (NodeToDelete.Value == nodeValue) {          // jesli wartosc sprawdzanego noda jest rowna szukanej wartosci
+
+					if (NodeToDelete.Previous == null) {         // jesli znaleziono wartosc na poczatku listy
+																 //Console.WriteLine("     Element do usuniecia znajduje sie na poczatku listy");
+						NodeToDelete.Next.Previous = null;
+						CurrentNode = NodeToDelete.Next;
+					}
+
+					else if (NodeToDelete.Next == null) {       // jesli znaleziono wartosc na koncu
+																//Console.WriteLine("     Element do usuniecia znajduje sie na końcu listy");
+						NodeToDelete.Previous.Next = null;
+						CurrentNode = NodeToDelete.Previous;
+					}
+
+					else {
+						//Console.WriteLine("     Element do usuniecia znajduje sie w środku listy");
+						NodeToDelete.Previous.Next = NodeToDelete.Next;
+						NodeToDelete.Next.Previous = NodeToDelete.Previous;
+						CurrentNode = NodeToDelete.Next;
+					}
+
+					//nodeFound = true;							// jeśli istotne jest sprawdzenie czy element do usunięcia występował na liście
+					Count--;
+				}
+
+				NodeToDelete = NodeToDelete.Next;               // przejdz na nastepny nod
+			}
+
+			//if (!nodeFound)									// jeśli istotne jest sprawdzenie czy element do usunięcia występował na liście
+			//	Console.WriteLine("     Elementu do usunięcia nie ma na liście");
+
+			//PrintListGD(ref CurrentNode, Count);				// aby umożliwić wypisanie wszystkich kroków
+		}
+
+		//================PrintListGD
+
+		private void PrintListGD(ref NodeGD node, int count) {
+
+			NodeGD PrintNode = node;
+			NodeGD temp = PrintNode;
+			while (PrintNode.Previous != null) {
+				temp = PrintNode;                             // do zmiennej tymczasowej leci aktualna pozycja PrintNode
+				PrintNode = PrintNode.Previous;               // do aktualnej pozycji PrintNode leci poprzednia pozycja PrintNode
+				PrintNode.Next = temp;                        // do następnej pozycji PrintNode leci zmienna tymczasowa
+			}
+
+			Console.Write("List: ");
+			while (PrintNode != null) {
+				Console.Write(PrintNode.Value + " ");
+				PrintNode = PrintNode.Next;
+			}
+			Console.WriteLine();
+		}
+
+		//================ExecuteListGD
+
+		public void ExecuteListGD(ParserGD parser) {
+
+			string[,] SubDataToParse = parser.GetSubDataEntries();
+			for (int i = 0; i < SubDataToParse.GetLength(0); i++) {
+				if (SubDataToParse[i, 0] == "i")
+					AddNodeGD(Int32.Parse(SubDataToParse[i, 1]), Int32.Parse(SubDataToParse[i, 2]));
+				if (SubDataToParse[i, 0] == "d")
+					DeleteNodeGD(Int32.Parse(SubDataToParse[i, 1]));
+			}
+
+			PrintListGD(ref CurrentNode, Count);
+		}
+	}
+}
---
+++ CwiczeniaGD5/ParserGD.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace CwiczeniaGD5
+{
+
+	class ParserGD {
+
+		string Path = "data.txt";
+		ArrayList ListToParse;
+		string[,] SubData;
+
+		public ParserGD() {
+			//new ParserGD(Path);
+			this.ListToParse = OpenFileSaveRawData(Path);
+			ParseData(ListToParse);
+		}
+
+		//public ParserGD(string Path) {
+		//	this.Path = Path;
+		//	new ParserGD();
+		//}
+
+		private ArrayList OpenFileSaveRawData(string Path) {            // SPRAWDZ CZY DANE SA POPRAWNE!
+
+			ArrayList RawData = new ArrayList();
+			string fileEntry;
+
+			try {
+				using (FileStream fileStream = new FileStream(Path, FileMode.Open, FileAccess.Read)) {
+					StreamReader streamReader = new StreamReader(fileStream);
+					while ((fileEntry = streamReader.ReadLine()) != "P") {
+						RawData.Add(fileEntry);
+					}
+				}
+			}
+			catch (Exception ex) {
+				Console.WriteLine("Wyjątek : " + ex.Message);
+				Console.WriteLine(@"Plik musi mieć nazwę data.txt i musi znajdować się w katalogu X:\Visual Studio\source\repos\ArrayListGD\ArrayListGD\bin\Debug\.");
+			}
+
+			return RawData;
+		}
+
+		private string[,] ParseData(ArrayList listToParse) {             // ArrayList jest skończona i nie będzie powiększana po uruchomieniu ParseData()
+
+			int EntryCount = 0;                                         // licznik linii dla ArrayList
+			foreach (string lineEntry in listToParse) {                 // dla każdej linii
+				EntryCount++;                                           // zwiększa entryCount
+			}
+			SubData = new string[EntryCount, 3];                            // deklaracja i zainicjalizowanie tablicy dwuwymiarowej SubData[,] wg entryCount
+			EntryCount = 0;                                             // wyzerowanie entryCount na potrzeby indeksowania SubData[][]
+
+			foreach (string lineEntry in listToParse) {                 // dla każdego wpisu w ArrayList
+				string[] SubDataEntry = lineEntry.Split('_');           // do subFileEntry wpisywana jest tylko jedna linia na raz
+				SubData[EntryCount, 0] = SubDataEntry[0];               // wstaw string z SubDataEntry[0] do Subdata[i][0]
+				SubData[EntryCount, 1] = SubDataEntry[1];               // wstaw string z SubDataEntry[0] do Subdata[i][0]
+				if (SubDataEntry[0] == "i") {                           // tylko jeśli pierwszy element Entry[] jest rowny i
+					SubData[EntryCount, 2] = SubDataEntry[2];           // wstaw string z SubDataEntry[2] do Subdata[i][2]
+				}
+				EntryCount++;                                           // przejście na kolejny indeks tablicy SubData[][]
+			}
+			return SubData;
+		}
+
+		public string[,] GetSubDataEntries() {
+			return SubData;
+		}
+	}
+
+
+}
